e94d5990d1dae4a86e9e1205242d3327549ac404
Yaksha69 <minato18q@gmail.com>
2025-06-27 11:31:40 +0800

    day2

---
+++ ramenmobileapp/lib/order_history.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OrderHistoryPage extends StatefulWidget {
+  const OrderHistoryPage({super.key});
+
+  @override
+  State<OrderHistoryPage> createState() => _OrderHistoryPageState();
+}
+
+class _OrderHistoryPageState extends State<OrderHistoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
